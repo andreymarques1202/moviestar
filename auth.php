20184596ed9203c6986808cf4927464a93d83189
Andrey Marques <andreymarques822@gmail.com>
2023-03-24 05:34:30 -0300

    Frontend: Criação e do formulario de login e cadastro

--- auth.php
+++ auth.php
@@ -8,30 +8,30 @@
             <div class="row" id="auth-row">
                 <div class="col-md-4" id="login-container">
                     <h2>Entrar</h2>
-                    <form action="" method="POST">
+                    <form action="<?= $BASE_URL?>auth_process.php" method="POST">
                     <input type="hidden" name="type" value="login">
                         <div class="form-group">
                             <label for="email">E-mail: </label>
-                            <input type="email" name="email" class="form-control" id="email" placeholder="Digite seu email">
+                            <input type="email" name="email" class="form-control" id="email" placeholder="Digite seu email" >
                         </div>
                         <div class="form-group">
                             <label for="password">Senha: </label>
-                            <input type="password" name="password" class="form-control" id="password" placeholder="Digite sua senha">
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Digite sua senha" >
                         </div>
                         <input type="submit" class="btn card-btn" value="Entrar">
                     </form>
                 </div>
                 <div class="col-md-4" id="register-container">
                     <h2>Criar Conta</h2>
-                    <form action="" method="post">
+                    <form action="<?= $BASE_URL?>auth_process.php" method="post">
                         <input type="hidden" name="type" value="register">
                         <div class="form-group">
                             <label for="email">E-mail: </label>
-                            <input type="email" name="email" class="form-control" id="email" placeholder="Digite seu email">
+                            <input type="email" name="email" class="form-control" id="email" placeholder="Digite seu email" >
                         </div>
                         <div class="form-group">
                             <label for="name">Nome: </label>
-                            <input type="text" name="name" class="form-control" id="name" placeholder="Digite seu nome">
+                            <input type="text" name="name" class="form-control" id="name" placeholder="Digite seu nome" >
                         </div>
                         <div class="form-group">
                             <label for="lastname">Sobrenome: </label>
@@ -39,11 +39,11 @@
                         </div>
                         <div class="form-group">
                             <label for="password">Senha: </label>
-                            <input type="password" name="password" class="form-control" id="password" placeholder="Digite sua senha">
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Digite sua senha" >
                         </div>
                         <div class="form-group">
                             <label for="confirmpassword">Confirmação de Senha: </label>
-                            <input type="password" name="confirmpassword" class="form-control" id="confirmpassword" placeholder="Confirme sua senha">
+                            <input type="password" name="confirmpassword" class="form-control" id="confirmpassword" placeholder="Confirme sua senha" >
                         </div>
                         <input type="submit"class="btn card-btn" value="Registrar">
                     </form>
